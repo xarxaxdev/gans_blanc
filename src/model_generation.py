a24c13cdc868fc3d3842e69ff83211a8b09aec9f
xarxaxdev <xarxax.dev@gmail.com>
2023-02-17 17:27:16 +0100

    you will see a conflict

--- src/model_generation.py
+++ src/model_generation.py
@@ -13,8 +13,31 @@ def test_roberta(sentences):
     #print(doc_1.similarity(doc_2[0:7]))
     print('------------doc1------------')
     for tok in doc_1:
-        print(tok.text, tok.pos_)
+        #print(tok.text, tok.pos_)
+        continute
+       
     print('------------doc2------------')
     for tok in doc_2:
         print(tok.text, tok.pos_)
         print(tok)
+
+
+"""
+###Here will be the main script for generating the models
+import spacy
+sentences = ['hello can i have some pizza',
+'do you want some tea']
+
+nlp = nlp = spacy.load('en_core_web_trf')
+
+# get two documents
+doc_1 = nlp(sentences[0])
+doc_2 = nlp(sentences[1])
+# use the similarity method that is based on the vectors, on Doc, Span or Token
+#print(doc_1.similarity(doc_2[0:7]))
+print('------------doc1------------')
+for tok in doc_1:
+    print(tok.text, tok.pos_)
+    print(doc_1._.trf_data.tensors)
+    
+"""
