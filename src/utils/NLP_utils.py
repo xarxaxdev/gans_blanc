9600efe271bbe07e688370f98a740766a907e471
doyouwantsometea <e920542@gmail.com>
2023-03-01 16:37:26 +0100

    Update NLP_utils.py
    debug for BIO tagging

--- src/utils/NLP_utils.py
+++ src/utils/NLP_utils.py
@@ -17,11 +17,17 @@ def bio(data):
     for i in range(0, len(tokenized)):
         for entity in data['annotations'][0]['result']:
             entity_token = word_tokenize(entity['value']['text'])
-            if tokenized[i] == entity_token[0]:
+            if tokenized[i] == entity_token[0] and len(entity_token) == 1:
                 tag[i] = 'B-' + entity['value']['labels'][0]
-                if len(entity_token) >= 2:
+
+            if tokenized[i] == entity_token[0] and len(entity_token) >= 2:
+                # last token of the entity
+                ending = len(entity_token)-1
+                if 0 < i+ending < len(tokenized) and tokenized[i+ending] == entity_token[ending]:
+                    tag[i] = 'B-' + entity['value']['labels'][0]
                     for j in range(1, len(entity_token)):
                         tag[i+j] = 'I-' + entity['value']['labels'][0]
+            
 
     print(data)
     print(tag)
