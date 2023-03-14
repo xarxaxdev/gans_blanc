c0d73297110d0e0dfb80557c6c554526940d9dc3
xarxaxdev <xarxax.dev@gmail.com>
2023-03-14 13:37:24 +0100

    gitignore changes

---
+++ src/model/roberta.py
@@ -0,0 +1,14 @@
+# pytorch libraries
+import torch # the main pytorch library
+import torch.nn as nn # the sub-library containing Softmax, Module and other useful functions
+import torch.optim as optim # the sub-library containing the common optimizers (SGD, Adam, etc.)
+
+# huggingface's transformers library
+from transformers import RobertaForTokenClassification, RobertaTokenizer
+
+# huggingface's datasets library
+from datasets import load_dataset
+
+# the tqdm library used to show the iteration progress
+import tqdm
+tqdmn = tqdm.notebook.tqdm
