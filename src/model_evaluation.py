20bcd1b3b64ba076fe6fb1a1a11aba4db2a2404a
doyouwantsometea <e920542@gmail.com>
2023-03-16 17:53:32 +0100

    added a simple built-in f1 method

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,2 +1,10 @@
 ### Here will be the main functions to evaluate the models
 ### Maybe draw some plots in src/plots
+
+import torch
+from torchmetrics.classification import MulticlassF1Score
+from model_generation import ent_to_ix
+
+def compute_f1(prediction, target):
+    metric = MulticlassF1Score(num_classes=len(ent_to_ix))
+    return metric(prediction, target)
