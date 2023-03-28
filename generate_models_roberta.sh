2fa4a458126ec25631fb43a8d19600abf9fe4b42
xarxaxdev <xarxax.dev@gmail.com>
2023-03-29 01:51:07 +0200

    removed old roberta plots

--- generate_models_roberta.sh
+++ generate_models_roberta.sh
@@ -2,15 +2,20 @@
 MODEL=roberta
 BATCHES=512
 EPOCHS=10
-DATASET=judgement
-for LR in 0.00001 0.00002 0.00005 0.0001
-do
-    python src/main.py --$MODEL --dataset $DATASET --epochs $EPOCHS --lr $LR --batch $BATCHES
-done
-
-#EPOCHS=10
 DATASET=preamble
 for LR in 0.00001 0.00002 0.00005 0.0001
 do
     python src/main.py --$MODEL --dataset $DATASET --epochs $EPOCHS --lr $LR --batch $BATCHES
 done
+
+DATASET=judgement
+for LR in 0.00001 0.00002 0.00005 0.0001
+do
+    for DATASET in preamble judgement
+    do
+        python src/main.py --$MODEL --dataset $DATASET --epochs $EPOCHS --lr $LR --batch $BATCHES
+    done
+done
+
+#EPOCHS=10
+
